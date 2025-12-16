f63169220cc94c59605a4a0a6980ac82cc02df90
praful-devv <your_github_email@example.com>
2025-12-16 09:28:06 +0530

    add like event

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+let flag = 0
 let like = document.querySelector('#like')
 like.addEventListener('click',function(){
-    like.style.backgroundColor = 'red'
+
+    if(flag === 0){
+        like.style.color = 'red'
+        flag = 1
+
+    }
+    else{
+         like.style.color = "white";
+         flag = 0;
+    }
+
 })
 
-
+let h1 = document.querySelector('h1')
 
 let arr = []
 let add = document.querySelector("#addfriend");
@@ -11,4 +22,20 @@ let h2 = document.querySelector("h2");
 add.addEventListener('click',function(){
     arr = prompt("name")
     h2.innerHTML = arr
+})
+
+let section = document.querySelector('section')
+section.addEventListener('dblclick',function(){
+    
+    if(flag === 0){
+
+        like.style.color = 'red'
+        like.style.color = 'red'
+        flag = 1
+    }
+    else{
+
+         like.style.color = "white";
+         flag = 0;
+    }
 })
